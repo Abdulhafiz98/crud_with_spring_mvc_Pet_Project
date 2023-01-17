5587286bd19fd8c02e0e1b0320f0ce0cbeab352d
Abdulkhafiz <abulhafizxalimjonov@gmail.com>
2023-01-17 13:56:06 +0500

    first changes

--- src/main/java/org/example/dao/mapper/UserMapper.java
+++ src/main/java/org/example/dao/mapper/UserMapper.java
@@ -10,15 +10,14 @@ import java.sql.SQLException;
 public class UserMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-        User user = User.builder()
+        return User.builder()
                 .id(rs.getInt("id"))
                 .name(rs.getString("name"))
                 .email(rs.getString("email"))
                 .password(rs.getString("password"))
                 .userRole(UserRole.valueOf(rs.getString("role")))
                 .phoneNumber(rs.getString("phone_number"))
-                .chatId(rs.getLong("chatId"))
+                .chatId(rs.getLong("chat_id"))
                 .build();
-        return user;
     }
 }
