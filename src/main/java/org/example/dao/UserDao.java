b2277e031139b2e3cd9513be46cce3c743ceca1b
asrorbek07 <asrorobidjonov2000@gmail.com>
2023-01-18 09:11:32 +0500

    added register form

--- src/main/java/org/example/dao/UserDao.java
+++ src/main/java/org/example/dao/UserDao.java
@@ -38,10 +38,18 @@ public class UserDao implements BaseDao<User> {
 
     @Override
     public boolean add(User user) {
-        return jdbcTemplate.update(
-                "insert into users(name, email, password, phone_number) values (?,?,?,?)",
-                new Object[]{user.getName(), user.getEmail(), user.getPassword(), user.getPhoneNumber()}
-        ) > 0;
+        int update=0;
+try {
+
+    update = jdbcTemplate.update(
+            "insert into users(name, email, password, phone_number,role) values (?,?,?,?,?)",
+            user.getName(), user.getEmail(), user.getPassword(), user.getPhoneNumber(), user.getUserRole().name());
+}
+catch (Exception ex){
+    ex.printStackTrace();
+}
+return update>0;
+
     }
 
     public User login(final UserLoginRequest userLoginRequest) {
