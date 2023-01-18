3beb33c85824be7c94b9e51efc1ef9096fe33f3d
Abdulkhafiz <abulhafizxalimjonov@gmail.com>
2023-01-18 14:05:11 +0500

    new changes

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -8,11 +8,11 @@ import org.example.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/auth")
@@ -46,14 +46,17 @@ public class AuthController {
     @PostMapping("/login")
     public String login(
             Model model,
-            @ModelAttribute UserLoginRequest loginRequest
-    ) {
+            @ModelAttribute UserLoginRequest loginRequest,
+            HttpServletResponse resp
+            ) {
         User currentUser = authService.login(loginRequest);
         model.addAttribute("message", "username or password is incorrect");
         model.addAttribute("isSuperAdmin", currentUser.getUserRole().equals(UserRole.SUPER_ADMIN));
         model.addAttribute("user", currentUser);
-        return "admin/index";
-
+        Cookie cookie = new Cookie("u_phone", currentUser.getPhoneNumber());
+        cookie.setMaxAge(5000);
+        resp.addCookie(cookie);
+        return currentUser.getUserRole().name().equals("USER") ? "user/home" : "admin/index";
     }
 
 
