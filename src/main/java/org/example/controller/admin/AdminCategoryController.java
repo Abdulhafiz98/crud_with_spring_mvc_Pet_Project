e70d3ed13da75987525891f619d15e15e7c13e12
asrorbek07 <asrorobidjonov2000@gmail.com>
2023-01-20 08:54:36 +0500

    added admin and seller adding form

--- src/main/java/org/example/controller/admin/AdminCategoryController.java
+++ src/main/java/org/example/controller/admin/AdminCategoryController.java
@@ -1,30 +1,36 @@
 package org.example.controller.admin;
 
+import org.example.dao.UserDao;
 import org.example.model.Category;
 import org.example.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 @RequestMapping("/admin/category")
 public class AdminCategoryController {
-
     private final CategoryService categoryService;
+    private final UserDao userDao;
 
-    public AdminCategoryController(CategoryService categoryService) {
+    public AdminCategoryController(CategoryService categoryService, UserDao userDao) {
         this.categoryService = categoryService;
+        this.userDao = userDao;
     }
 
     @GetMapping("/list")
     public String getCategoryList(
-            Model model
+            Model model,
+            HttpServletRequest httpServletRequest
     ){
+        HttpSession session = httpServletRequest.getSession();
+        Integer userId = (Integer)session.getAttribute("userId");
+        model.addAttribute("userRole",userDao.getById(userId).getUserRole().name());
         List<Category> categoryList = categoryService.getCategoryList();
         model.addAttribute("categoryList", categoryList);
         return "admin/category";
