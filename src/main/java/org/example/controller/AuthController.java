e0720bf0ca25252cb6d13b5f7a858b1600f8eaff
Abdulkhafiz <abulhafizxalimjonov@gmail.com>
2023-01-21 18:12:41 +0500

    Log out button added

--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 
+
 import org.example.dao.UserDao;
 import org.example.dto.UserLoginRequest;
 import org.example.dto.UserRegisterRequest;
@@ -9,6 +10,7 @@ import org.example.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -85,5 +87,12 @@ public class AuthController {
         model.addAttribute("isSuperAdmin", currentUser.getUserRole() != null && currentUser.getUserRole().name().equals(UserRole.SUPER_ADMIN.name()));
         model.addAttribute("user", currentUser);
         return currentUser.getUserRole().name().equals("USER") ? "web/index" : "admin/index";
+    }
+
+    @GetMapping("/logOut")
+    public String logOut(HttpServletRequest req){
+        req.getSession().setAttribute("userId",null);
+        req.getSession().setMaxInactiveInterval(0);
+        return "/index";
     }
 }
